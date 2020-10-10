d8cf2c0e4a98db78a6ee60b89f105165071e74ce
salemalwatry <www.salem.alwatry@gmail.com>
2020-10-10 20:50:27 +0300

    الواجب خلص

--- scripts.js
+++ scripts.js
@@ -3,11 +3,28 @@
 
 // 2
 // "قم بالوصول إلى كلمة الأرض وتحويلها إلى "القمر
-
+let dark=false;
 // 3
 // بحيث إن ضغطت على البطاقة تتغير الصورة للصورة الأخرى ويتغير الاسم onClick قم بإنشاء دالة واستخدام خاصية
 // دون الحاجة إلى الخطوتين 2و1
+function planetSwitcher() {
+    let title = document.getElementById("caption");
+    let image =document.getElementById("image")
+    let body = document.getElementById("body");
+    dark=!dark;
 
+   if (dark==false){
+       body.style.backgroundColor="white"
+       image.src ="./images/sun.jpg"
+        title.innerHTML= "الشمس"
+        body.style.transition="1s ease"
+   }
+   else{
+       body.style.backgroundColor="gray"
+       image.src ="./images/moon.jpg"
+       title.innerHTML="القمر"
+   }
+}
 // 4
 // Night - Light Mode قم بالتعديل على الدالة بحيث تضم إليها خاصية
 
